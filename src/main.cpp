41fe00c21d764865ee45944c401a321ce02b870d
Daniel Pelzel <pelzel.daniel@gmail.com>
2026-03-18 21:19:13 +0100

    Hardwaretest, indem Lampe zum blinken gebracht wird

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,21 @@
-Daniel 
+#include <Arduino.h>
+bool  an = false;
+
+void setup() {
+
+    pinMode(13 , OUTPUT);
+
+}
+
+
+void loop() {
+
+     //LED-Soll blinken
+    an = true;
+    digitalWrite(13, HIGH);
+    delay(10);
+    an = false;
+    digitalWrite(13, LOW);
+    delay(1000);
+
+}
