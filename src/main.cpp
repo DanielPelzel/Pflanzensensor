40bc0ebb998eaf57becfc478c2fad215cfef329e
Daniel Pelzel <pelzel.daniel@gmail.com>
2026-03-18 22:02:31 +0100

    Code geschrieben um: Feuchtigkeit zu messen und Lampe zum leuchten zu bringen wenn diese unter 20% fällt

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,33 @@
 #include <Arduino.h>
+
+int feuchtigkeit = 0;
+int feuchtigkeit_prozent = 0;
 bool  an = false;
 
 void setup() {
 
+    //Pins belegen
     pinMode(13 , OUTPUT);
+    pinMode(A0, INPUT);
+    Serial.begin(9600);
 
 }
 
-
 void loop() {
 
-     //LED-Soll blinken
-    an = true;
-    digitalWrite(13, HIGH);
-    delay(10);
-    an = false;
-    digitalWrite(13, LOW);
-    delay(1000);
+    //Misst Feuchtigkeit und sendet Prozentwert an Computer
+    feuchtigkeit = analogRead(A0);
+    feuchtigkeit_prozent  = map(feuchtigkeit, 464, 191, 0, 100);
+    Serial.println(feuchtigkeit_prozent);
 
+
+    //Lässt die Lampe leuchten wenn die Bodenkapazität unter 20% fällt
+
+    if (feuchtigkeit_prozent < 20) {
+        digitalWrite(13, HIGH);
+    }else {
+        digitalWrite(13, LOW);
+    }
+    // kurze Pause damit nicht zu viele Daten an Computer geschickt werden
+    delay(1000);
 }
